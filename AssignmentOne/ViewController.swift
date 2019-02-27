1db2f06f2424b5069329397f4b6a8ecb1551c26d
Jahan <JahanUoL@users.noreply.github.com>
2019-02-27 14:33:43 +0000

    Initial Commit

---
+++ AssignmentOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentOne
+//
+//  Created by Jahan on 27/02/2019.
+//  Copyright © 2019 Jahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
