00d7434e6715e30c0adc07f517f1eb94f66d61e8
Jahan <JahanUoL@users.noreply.github.com>
2019-03-06 12:40:23 +0000

    clean ups/comments
    About to start adding favorites in

--- AssignmentOne/ViewController.swift
+++ AssignmentOne/ViewController.swift
@@ -10,13 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController  {
     
-    var reportsArray : [techReport]! // Stores all the reports from the JSON
     var jahanReport = [[techReport]]() // first array stores years, second year stores the reports associated with them
-    
-    // Have an array that stores each years count, so years[0] will store the amount of 2001, ex - 8.
-    // if the current year selected is not equal to the previous year, inc counter, and move onto the next year (2002).
-    // counter += 1
-    // years[counter]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,43 +19,27 @@ class ViewController: UITableViewController  {
         
     }
     
-    func array() {
-        var count = 0
-        jahanReport.append([techReport]())
-        jahanReport[0].append(reportsArray[0])
-        for i in 1..<reportsArray.count {
-            if (reportsArray?[i-1].year != reportsArray?[i].year) {
-                count += 1
-                jahanReport.append([techReport]())
-            }
-            jahanReport[count].append(reportsArray[i])
-        }
-    }
-    
     // Gets the title header for each section, by accessing the first array from the 2D which stores the years for every associated report
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return jahanReport[section].first?.year
     }
-    
+    // returns 18 as the range of years is from 2001 - 2018
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return jahanReport.count // returns 18 as there is 2001 - 2018 number of sections
+        return jahanReport.count // returns the count of the first array
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(jahanReport[section].count)
         return jahanReport[section].count // Gets the count of how many papers there are given the year
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "jsonCells", for: indexPath)
-        
-        if !jahanReport.isEmpty{
-            let report = jahanReport[indexPath.section][indexPath.row]
-            cell.textLabel?.text = " \(report.year) -- \(report.authors) -- \(report.title)"
-        }
-        
+        let report = jahanReport[indexPath.section][indexPath.row]
+        cell.textLabel?.text = report.title ?? "No title"
+        cell.detailTextLabel?.text = report.authors ?? "No authors"
         return cell
     }
+    
     
     func decodeJson() {
         if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/techreports/data.php?class=techreports2") {
@@ -74,8 +52,8 @@ class ViewController: UITableViewController  {
                 do {
                     let decoder = JSONDecoder()
                     let getTechnicalReports = try decoder.decode(technicalReports.self, from: jsonData)
-                    self.reportsArray = getTechnicalReports.techreports2.sorted(by: {$0.year > $1.year })
-                    self.array()
+                    let sortedReports = getTechnicalReports.techreports2.sorted(by: {$0.year > $1.year })
+                    self.array(reportsArray: sortedReports)
                     DispatchQueue.main.async(execute: {
                         self.tableView.reloadData()
                     })
@@ -86,6 +64,20 @@ class ViewController: UITableViewController  {
                 }
                 }
                 .resume()
+        }
+    }
+    
+    func array(reportsArray: [techReport]) {
+        var count = 0 // Stores the element position of the year array within the 2D array
+        jahanReport.append([techReport]()) // Initalising/Appending array to store type techReport
+        jahanReport[0].append(reportsArray[0])
+
+        for i in 1..<reportsArray.count {
+            if (reportsArray[i-1].year != reportsArray[i].year) { // If prev reports year is different to current reports year.
+                count += 1
+                jahanReport.append([techReport]())
+            }
+            jahanReport[count].append(reportsArray[i]) // Adds current report selected to the correct associated year
         }
     }
 }
